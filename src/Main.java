16d866e7977a3839db5a3d43f1fac78f8668ff2f
Kunszt Norbert <kunszt.norbert@gmail.com>
2022-12-22 18:17:06 +0100

    Fixed!!

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(palindrom("arra"));
+        System.out.println(palindrom("kék"));
     }
     public static boolean palindrom(String text){
-        StringBuilder rev = new StringBuilder();
-        for (int i = text.length()-1; i >= 0; i--) {
-            rev.append(text.charAt(i));
-        }
-        System.out.println("rev: "+rev);
-        System.out.println("text: "+text);
-        return rev.equals(text);
+        StringBuilder rev = new StringBuilder(text);
+       rev.reverse();
+        return rev.toString().equals(text);
 
     }
 }
